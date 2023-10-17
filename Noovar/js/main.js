01eb402f69a27e1e80efff907f1000c97458c83a
RafaelFerreira07 <140099097+RafaelFerreira07@users.noreply.github.com>
2023-10-16 22:44:07 -0300

    Delete Noovar directory

--- Noovar/js/main.js
+++
@@ -1,7 +0,0 @@
-function redirecionarParaURL(url) {
-    window.location.href = url;
-  }
-
-document.getElementById('logo').addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
